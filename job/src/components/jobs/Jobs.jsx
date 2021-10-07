b61d818ca31ae2d05fc23fc3628b7fb9ab3feb7a
moise <mose.ns@hotmail.com>
2021-10-07 14:39:53 +0200

    position component

--- job/src/components/jobs/Jobs.jsx
+++ job/src/components/jobs/Jobs.jsx
@@ -1,35 +1,53 @@
 import './jobs.scss';
+import { data } from '../../data/data';
+import Position from './Position';
+
 
 const Jobs = () => {
+  
+
+
   return (
     <div className='jobs-container'>
-      <div className='job-list-container'>
-        <div className='position-container'>
-          <div className='logo-company'>logo</div>
-          <div className='position-content'>
-            <div className="company">
-              <p className='company-name'>Photosnap</p>
-              <p className='company-new'>NEW!</p>
-              <p className='company-featured'>FEATURED</p>
-            </div>
-            <p className='position-title'>Senior Front-End Developer</p>
-            <div className='position-infos'>
-              <p>1day ago</p>
-              <p>.</p>
-              <p>Full Time</p>
-              <p>.</p>
-              <p>USA only</p>
+      {data.map(job => {
+        const { 
+          id, 
+          company, 
+          logo, 
+          news, 
+          featured, 
+          position, 
+          role, 
+          level, 
+          postedAt, 
+          contract, 
+          location, 
+          languages, 
+          tools 
+        } = job;
+        return (
+          <div className='job-list-container' key={id}>
+            <Position 
+              company={company} 
+              logo={logo} 
+              news={news} 
+              featured={featured} 
+              position={position} 
+              postedAt={postedAt}  
+              contract={contract}
+              location={location}
+
+            />
+            <div className='position-categories-container'>
+              <p className='categories-bgColor active-category'>Frontend</p>
+              <p className='categories-bgColor'>Senior</p>
+              <p className='categories-bgColor'>HTML</p>
+              <p className='categories-bgColor'>CSS</p>
+              <p className='categories-bgColor'>JAVASCRIPT</p>
             </div>
           </div>
-        </div>
-        <div className='position-categories-container'>
-          <p className='categories-bgColor active-category'>Frontend</p>
-          <p className='categories-bgColor'>Senior</p>
-          <p className='categories-bgColor'>HTML</p>
-          <p className='categories-bgColor'>CSS</p>
-          <p className='categories-bgColor'>JAVASCRIPT</p>
-        </div>
-      </div>
+        )
+      })}
     </div>
   )
 }
